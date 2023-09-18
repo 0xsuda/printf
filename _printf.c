d41dbfe68480f039cea673ab17d9d38d8a92c9fe
0xsuda <osama.suliman@protonmail.com>
2023-09-18 11:57:46 -0700

    switch with no i d

--- _printf.c
+++ _printf.c
@@ -6,55 +6,49 @@
  * _printf -function that produces output according to a format.
  * @format: The number of paramters
  * @...: A variable number of paramters
- *
- *
  * Return: int
  */
-typedef struct {
-char specifier;
-void (*handler)(va_list);
-} conversion_handler;
-
-void handle_char(va_list args) {
-char c = va_arg(args, int);
-_putchar(c);
-}
-void handle_perc(va_list args) {
-    (void)args;
-    _putchar('%');
-}
-void handle_string(va_list args) {
-char *s = va_arg(args, char *);
-while (*s != '\0') {
-_putchar(*s++);
-}
-}
-conversion_handler handlers[] = {
-{'c', handle_char},
-{'s', handle_string},
-{'%', handle_perc},
-};
 int _printf(const char *format, ...)
 {
-int  count = 0;
+char *arg_s;
+char arg_c;
+int count = 0;
 va_list args;
-char conversion_specifier;
-long unsigned int i ;
+char conversion_specifiers;
+
 if (format == NULL)
 return (-1);
 va_start(args, format);
-while ((conversion_specifier = *format++) != '\0') {
-if (conversion_specifier == '%') {
-conversion_specifier = *format++;
- for (i = 0; i < sizeof(handlers)/sizeof(conversion_handler); i++) {
-if (handlers[i].specifier == conversion_specifier) {
-handlers[i].handler(args);
+while ((conversion_specifiers = *format++) != '\0')
+{
+if (conversion_specifiers == '%')
+{
+switch (*format++)
+{
+case 'c':
+arg_c = va_arg(args, int);
+_putchar(arg_c);
 count++;
+break;
+case 's':
+arg_s = va_arg(args, char *);
+while (*arg_s != '\0')
+{
+_putchar(*arg_s++);
+count++;
+}
+break;
+case '%':
+_putchar('%');
+count++;
+break;
+default:
 break;
 }
 }
-} else {
-_putchar(conversion_specifier);
+else
+{
+_putchar(conversion_specifiers);
 count++;
 }
 }
