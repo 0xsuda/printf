1dd1ced1fd21c88c83347e415ca17d880269623d
0xsuda <osama.suliman@protonmail.com>
2023-09-19 15:29:33 -0700

    printf function

---
+++ _printf.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _printf - function to print the required output.
+ * @format: identifer.
+ * Return: output length.
+ */
+int _printf(const char * const format, ...)
+{
+	convert_match m[] = {
+		{"%s", printf_string}, {"%c", printf_char},
+		{"%%", printf_37},
+		{"%i", printf_int}, {"%d", printf_dec},
+	};
+
+	va_list args;
+	int i = 0, j, len = 0;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+Here:
+	while (format[i] != '\0')
+	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
+			{
+				len += m[j].f(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		len++;
+		i++;
+	}
+	va_end(args);
+	return (len);
+}
