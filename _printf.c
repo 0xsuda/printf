b3760ddb7b966494f86894763ae1416c3373d749
0xsuda <osama.suliman@protonmail.com>
2023-09-19 16:11:32 -0700

    update printf

--- _printf.c
+++ _printf.c
@@ -15,13 +15,6 @@ int (*find_function(const char *format))(va_list)
 		{"s", print_string},
 		{"i", print_int},
 		{"d", print_dec},
-		{"r", print_rev},
-		{"b", print_bin},
-		{"u", print_unsig},
-		{"o", print_octal},
-		{"x", print_x},
-		{"X", print_X},
-		{"R", print_rot13},
 		{NULL, NULL}
 	};
 
