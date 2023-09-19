888c955c9a7b719d9fc01eb2cd7906a0cfe6c128
asma <asmaa.alhadi100@gmail.com>
2023-09-19 15:49:15 -0700

    edit to the main.h file

--- main.h
+++ main.h
@@ -1,3 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+
+
+
 /**
  * struct format - match the conversion specifiers for printf
  * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
@@ -34,3 +46,4 @@ int printf_char(va_list val);
 int printf_string(va_list val);
 int _putchar(char c);
 int _printf(const char *format, ...);
+#endif
